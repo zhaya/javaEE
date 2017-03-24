74bfabfabff5d8246436f1ae3f6014231e456693
赵焱 <15637138736@163.com>
2017-03-24 22:05:51 +0800

    Add files via upload
    每行包行两个整数a和b
    对于每行输入对应输出一行a和b的和

---
+++ ACM/Main.java
@@ -0,0 +1,13 @@
+package niukeCode;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String [] args){
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNext()){
+			System.out.println(scan.nextLine()+scan.nextLine());
+		}
+		
+	}
+}
